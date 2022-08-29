b41cf0e8904f864f350e3cba556045ba16c674e0
shaoyuancc <shaoyuan.chewchia@letsbramble.com>
2022-08-29 09:09:14 -0400

    linked list implementation

--- hash_table.h
+++ hash_table.h
@@ -1,17 +1,25 @@
 #ifndef CS165_HASH_TABLE // This is a header guard. It prevents the header from being included more than once.
-#define CS165_HASH_TABLE  
+#define CS165_HASH_TABLE
 
-typedef struct hashtable {
-// define the components of the hash table here (e.g. the array, bookkeeping for number of elements, etc)
+typedef struct hashtable
+{
+    // define the components of the hash table here (e.g. the array, bookkeeping for number of elements, etc)
 } hashtable;
 
 typedef int keyType;
 typedef int valType;
 
-int allocate(hashtable** ht, int size);
-int put(hashtable* ht, keyType key, valType value);
-int get(hashtable* ht, keyType key, valType *values, int num_values, int* num_results);
-int erase(hashtable* ht, keyType key);
-int deallocate(hashtable* ht);
+typedef struct node
+{
+    keyType key;
+    valType val;
+    struct node *next;
+} node;
+
+int allocate(hashtable **ht, int size);
+int put(hashtable *ht, keyType key, valType value);
+int get(hashtable *ht, keyType key, valType *values, int num_values, int *num_results);
+int erase(hashtable *ht, keyType key);
+int deallocate(hashtable *ht);
 
 #endif
--- main.c
+++ main.c
@@ -4,8 +4,30 @@
 #include "hash_table.h"
 
 // This is where you can implement your own tests for the hash table
-// implementation. 
-int main(void) {
+// implementation.
+int main(void)
+{
+  node *head = NULL;
+  node *second = NULL;
+  node *third = NULL;
+
+  head = (node *)malloc(sizeof(node));
+  second = (node *)malloc(sizeof(node));
+  third = (node *)malloc(sizeof(node));
+
+  head->key = 1;
+  head->val = 10;
+  head->next = second;
+
+  second->key = 2;
+  second->val = 20;
+  second->next = third;
+
+  third->key = 3;
+  third->val = 30;
+  third->next = NULL;
+
+  printf("Hi this is running third key %d val %d", head->next->next->key, head->next->next->val);
 
   hashtable *ht = NULL;
   int size = 10;
@@ -18,17 +40,19 @@ int main(void) {
 
   int num_values = 1;
 
-  valType* values = malloc(1 * sizeof(valType));
+  valType *values = malloc(1 * sizeof(valType));
 
-  int* num_results = NULL;
+  int *num_results = NULL;
 
   get(ht, key, values, num_values, num_results);
-  if ((*num_results) > num_values) {
+  if ((*num_results) > num_values)
+  {
     values = realloc(values, (*num_results) * sizeof(valType));
     get(ht, 0, values, num_values, num_results);
   }
 
-  for (int i = 0; i < (*num_results); i++) {
+  for (int i = 0; i < (*num_results); i++)
+  {
     printf("value of %d is %d \n", i, values[i]);
   }
   free(values);
